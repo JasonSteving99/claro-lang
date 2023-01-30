3827986a3da1abad2bdedba6230051303c81aaa3
Jason Steving <jasonmail99@gmail.com>
2023-01-30 11:38:28 -0800

    Support Trashcan Assignment Stmt
    This new statement allows you to dump a value "into the trash", indicating that you'll never make use of the value. This is primarily intended for use when calling some side effecting provider/function whose value you don't care about. But it can also be useful within procedure defs where you don't actually want to make use of a parameter value (yet).
    
    This statement is intended to serve as an explicit indication that a value is being thrown away. This is "syntactic salt" (as opposed to "sugar") as this is intended to serve as a very visible indication that you should validate you're throwing this value away on purpose rather than on accident.
    
    This has been long-awaited in the language. As Claro requires all variables to be used at least once after initialization, you previously needed to eventually have every single value in the program funnel down into a print statement directly or indirectly as `print(...)` was really the only statement in the entire language that could swallow a value (there was a hack where you could also fmt strings into a trash string and then both strings are technically "used" w/o printing, but obviously this was still disgustingly hacky).

--- mdbook-docs/src/common_programming_concepts/variables_and_types.md
+++ mdbook-docs/src/common_programming_concepts/variables_and_types.md
@@ -3,17 +3,25 @@
 Claro is a statically-compiled, strictly typed language. Practically speaking, this means that the type of all variables
 must be statically determined upon declaration of the variable, and may never change thereafter.
 
-Claro has a few builtin primitive types (representing generally small or low-level "value types" that are immutable to
-the programmer). These values are generally cheap to allocate on the stack, and are passed as copies to other functions.
-More are coming soon, but for now the supported set of primitives include: int, float, string, boolean. The example
+Claro has a few builtin "primitive" types representing generally small or low-level "value types" that are immutable to
+the programmer. They are referred to as "primitive" because they are foundational to the language's type system, and
+make up the basic building blocks of which every other type in the language is just some structured combination. Values
+of these primitive types are generally cheap to allocate on the stack, and are passed as copies to other functions
+(strings, being handled in typical JVM fashion, are actually heap allocated with references to strings passed instead of
+copying the value itself).
+
+More are coming soon, but for now the supported set of primitives include: int, float, boolean, string. The example
 below shows how you'd define variables to represent values of each type:
 
 ```
+# All immutable.
 var i: int = 10; # Any whole number. 
 var f: float = 1.15; # Any decimal number.
-var s: string = "very first string"; # Any sequence of chars.
 var b: boolean = true; # true or false.
+var s: string = "very first string"; # Any sequence of chars. Heap allocated.
 ```
+
+To break the syntax down further:
 
 `var` : Keyword introducing / declaring a new variable.
 
--- src/java/com/claro/intermediate_representation/statements/TrashcanAssignmentStmt.java
+++ src/java/com/claro/intermediate_representation/statements/TrashcanAssignmentStmt.java
@@ -1,35 +1,45 @@
 package com.claro.intermediate_representation.statements;
 
 import com.claro.compiler_backends.interpreted.ScopedHeap;
+import com.claro.intermediate_representation.expressions.Expr;
 import com.claro.intermediate_representation.expressions.term.IdentifierReferenceTerm;
 import com.claro.intermediate_representation.types.ClaroTypeException;
 import com.google.common.collect.ImmutableList;
 
 public class TrashcanAssignmentStmt extends Stmt {
-  private final IdentifierReferenceTerm identifier;
+  private final Expr expr;
 
-  public TrashcanAssignmentStmt(IdentifierReferenceTerm identifier) {
+  public TrashcanAssignmentStmt(Expr expr) {
     super(ImmutableList.of());
-    this.identifier = identifier;
+    this.expr = expr;
   }
 
   @Override
   public void assertExpectedExprTypes(ScopedHeap scopedHeap) throws ClaroTypeException {
     // This is the trashcan... you can throw literally anything into the trash if you for some
     // reason feel like that's a good idea. Just mark the identifier used.
-    identifier.getValidatedExprType(scopedHeap);
-    // TODO(steving) Need to do this right. Need to actually make sure that you can't rereference this variable after.
+    this.expr.getValidatedExprType(scopedHeap);
   }
 
   @Override
   public GeneratedJavaSource generateJavaSourceOutput(ScopedHeap scopedHeap) {
-    scopedHeap.markIdentifierUsed(this.identifier.getIdentifier());
-    return GeneratedJavaSource.forJavaSourceBody(new StringBuilder());
+    if (this.expr instanceof IdentifierReferenceTerm) {
+      scopedHeap.markIdentifierUsed(((IdentifierReferenceTerm) this.expr).getIdentifier());
+      return GeneratedJavaSource.forJavaSourceBody(new StringBuilder());
+    } else { // Here we've got a provider/function call whose side effect we want to keep, but ignore the result value.
+      GeneratedJavaSource res = this.expr.generateJavaSourceOutput(scopedHeap);
+      res.javaSourceBody().append(';');
+      return res;
+    }
   }
 
   @Override
   public Object generateInterpretedOutput(ScopedHeap scopedHeap) {
-    scopedHeap.markIdentifierUsed(this.identifier.getIdentifier());
+    if (this.expr instanceof IdentifierReferenceTerm) {
+      scopedHeap.markIdentifierUsed(((IdentifierReferenceTerm) this.expr).getIdentifier());
+    } else { // Here we've got a provider/function call whose side effect we want to keep, but ignore the result value.
+      this.expr.generateInterpretedOutput(scopedHeap);
+    }
     return null;
   }
 }
